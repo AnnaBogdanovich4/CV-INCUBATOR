a2ceb6642894724460eb19135e0c150f804707b7
Anna Bogdanovich <bogdanovicanna4@gmail.com>
2023-04-17 21:11:48 +0300

    Create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./header/Header";
+import {Main} from "./main/Main";
+import {AboutMe} from "./aboutme/AboutMe";
+import {Skills} from './skills/Skills'
+import {MyExperienceAndEducation} from "./myExpertAndExam/MyExperienceAndEducation";
+import {MyProjects} from "./myProjects/MyProjects";
+import {Contact} from "./contact/Contact";
+import {Footer} from "./footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header/>
+            <Main/>
+            <AboutMe/>
+            <Skills/>
+            <MyExperienceAndEducation/>
+            <MyProjects/>
+            <Contact/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
